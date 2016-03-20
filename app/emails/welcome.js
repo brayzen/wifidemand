f111a93b606c2cee735c3ec5789cec34fe72c4e2
brayzen <brian.ray.biz@gmail.com>
2016-03-20 14:01:13 -0700

    emails working as they should

--- app/emails/welcome.js
+++ app/emails/welcome.js
@@ -1,19 +1,26 @@
-var html =
+module.exports = {
+  email: function(name, email){
+    return {
+        to: email,
+        from: process.env.FROM_EMAIL,
+        // from: 'brayzenone@gmail.com',
+        subject: 'Welcome to Cooperative Wifi',
+        html: '<h1>Welcome from Raylife3</h1>' +
 
-  '<h1>Welcome from Raylife3</h1>' +
+              '<p> Hi ' + name + ',</p>' +
 
-  "<p> Thanks for signing up, you've been added to our customer list and we'll be contacting you as soon as we make any progress towards bringing your community better internet</p>" +
+              "<p> Thanks for signing up, you've been added to our customer list and we'll be contacting you as soon as we make any progress towards bringing your community better internet</p>" +
 
-  "<p> If you liked to know more about the technology we'll employ, please visit <a href='#'>this link</a></p>" +
+              "<p> If you liked to know more about the technology we'll employ, please visit <a href='#'>this link</a></p>" +
 
-  "<p> In the meantime you can help speed up the process by forwarding your friends, neighbors, and community this email.</p>" +
+              "<p> In the meantime you can help speed up the process by forwarding your friends, neighbors, and community this email.</p>" +
 
-  "<p> If you prefer Facebook <a href='#'><button>HERE</button></a><p>" +
+              "<p> If you prefer Facebook <a href='#'><button>HERE</button></a><p>" +
 
-  "<p> Sincerely,</p>" +
+              "<p> Sincerely,</p>" +
 
-  "<p> Raylife </p>"
+              "<p> Raylife </p>"
+    }
+  }
+}
 
-
-
-module.exports = {html: html};
